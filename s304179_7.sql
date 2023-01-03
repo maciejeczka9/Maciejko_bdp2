a63be37058693fb68ae00e6b0c3f0f3143d64dde
maciejeczka9 <61703179+maciejeczka9@users.noreply.github.com>
2023-01-03 21:03:23 +0100

    Add files via upload

---
+++ s304179_7.sql
@@ -0,0 +1,13 @@
+CREATE TABLE AdventureWorksDW2019.dbo.CSV_Customers
+(FirstName varchar,
+LastName varchar,
+EmailAddress varchar,
+Address varchar,
+City varchar,
+Region varchar,
+PhoneNumber varchar,
+CREATE_TIMESTMP datetime,
+UPDATE_TIMESTAMP datetime );
+
+
+SELECT* FROM AdventureWorksDW2019.dbo.CSV_Customers;
